8a31eb380550fb81deced0e32e9078ef718cc03a
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-08-01 00:25:01 +0800

    feat(component): setup external link

---
+++ src/components/external-link/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { NSpace, NIcon } from 'naive-ui';
+import { LogoGithub } from '@vicons/carbon';
+import styles from './index.module.scss';
+
+const GITHUB_REPO_LINK = 'https://github.com/Lmmmmmm-bb/currency-conversion/';
+</script>
+
+<template>
+  <n-space :class="styles.wrapper">
+    <a
+      title="GitHub"
+      target="_blank"
+      :class="styles.logo"
+      :href="GITHUB_REPO_LINK"
+    >
+      <n-icon :size="24">
+        <logo-github />
+      </n-icon>
+    </a>
+  </n-space>
+</template>
