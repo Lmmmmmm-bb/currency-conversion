416b0b6296b3b4460a9276fc91e8a5ba0db1b923
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-08-01 00:26:00 +0800

    chore: external link in main layout

--- src/app/index.vue
+++ src/app/index.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import ExternalLink from '../components/external-link';
+import styles from './index.module.scss';
+</script>
+
 <template>
-  <div>Hello</div>
+  <div :class="styles.wrapper">
+    <div :class="styles.main">Hello</div>
+    <external-link :class="styles.socialLink" />
+  </div>
 </template>
