c0288beb01518a204567e8415f36c57b84ac13a0
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-07-31 20:14:54 +0800

    chore: rename project to currency-conversion

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/currency-rate-transform/',
+  base: '/currency-conversion/',
   plugins: [vue()],
   resolve: {
     alias: {
