8173f5a86129887cb7b5e7d5807bc58939a04e7f
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-10-05 09:46:10 +0800

    refactor(og): open graph plugin support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,27 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import ogPlugin, { Options } from 'vite-plugin-open-graph';
+
+const ogOptions: Options = {
+  basic: {
+    title: 'Currency Conversion',
+    url: 'https://convert.lmmmmmm.dev',
+    description:
+      'Help you convert, use vue, conversion information provided by exchangerate.host.'
+  },
+  twitter: {
+    title: 'Currency Conversion',
+    card: 'summary',
+    description:
+      'Help you convert, use vue, conversion information provided by exchangerate.host.',
+    creator: '@_lmmmmmm'
+  }
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), ogPlugin(ogOptions)],
   resolve: {
     alias: {
       '~': resolve(__dirname, 'src')
