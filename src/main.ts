432aa0b6b24f564600966cf16c44858b2832dd26
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-01-19 00:03:05 +0800

    feat: vercel analytics support

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
-import { createApp } from 'vue';
 import 'reset-css';
+import { createApp } from 'vue';
+import { inject } from '@vercel/analytics';
+
 import './index.scss';
 import App from './app/index.vue';
+
+inject();
 
 const app = createApp(App);
 
